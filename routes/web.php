681d90b29ef8203dec680ead9f8440a3c6364277
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2023-10-07 18:54:29 -0400

    Ruta de eben

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::get('/dashboard', [DefaultController::class, 'index'])->name('dashboard')
 Route::get('/dashboard/clientes', [ClientesController::class, 'index'])->name('clientes.index');
 Route::get('/dashboard/personal', [PersonalController::class, 'index'])->name('personal.index');
 Route::get('/dashboard/personal/cargo', [CargoController::class, 'index'])->name('cargo.index');
+Route::get('/dashboard/vehiculos', [CargoController::class, 'index'])->name('vehiculos.index');
+
