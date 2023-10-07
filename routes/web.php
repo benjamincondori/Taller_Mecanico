2b57326c8a6ca5eb1aa0e46b03c2f882ed5b58c0
Alexis Lijeron <xmrpablitox15@hotmail.com>
2023-10-07 18:43:22 -0400

    listo

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/dashboard', [DefaultController::class, 'index'])->name('dashboard')
 Route::get('/dashboard/clientes', [ClientesController::class, 'index'])->name('clientes.index');
 Route::get('/dashboard/personal', [PersonalController::class, 'index'])->name('personal.index');
 Route::get('/dashboard/personal/cargo', [CargoController::class, 'index'])->name('cargo.index');
+Route::get('/dashboard/vehiculos', [VehiculoController::class, 'index'])->name('vehiculos.index');
