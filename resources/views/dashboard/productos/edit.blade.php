6beceb6b3220f934ef389334eca627fd10f77c83
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2023-10-26 06:46:39 -0400

    Gestionar Productos

--- resources/views/dashboard/productos/edit.blade.php
+++ resources/views/dashboard/productos/edit.blade.php
@@ -7,32 +7,21 @@
                 <div class="card-box">
 
                     <div class="form-group px-4 pt-2">
-<<<<<<< HEAD
-                        <i class="fas fa-circle-plus fa-2x"></i>
-                        <h3 class="fs-1 d-inline-block ml-1">Editar producto</h3>
-                    </div>
-
-                    <form class="px-4 pt-2 pb-2" action="{{ route('productos.update',  $producto['id']) }}" method="post" enctype="multipart/form-data">
-=======
                         <i class="fas fa-pencil-alt fa-2x"></i>
                         <h3 class="fs-1 d-inline-block ml-1">Editar producto</h3>
                     </div>
 
                     <form class="px-4 pt-2 pb-2" action="{{ route('productos.update', $producto['id']) }}"
                         method="post">
->>>>>>> benjamin
+
                         @csrf
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="nombre" class="control-label">Nombre</label>
                                     <input type="text" class="form-control" id="nombre" name="nombre"
-<<<<<<< HEAD
-                                        placeholder="Liquido de freno" value="{{ old('nombre', $producto['nombre']) }}">
-=======
                                         placeholder="Liquido de freno"
                                         value="{{ old('nombre', $producto['nombre']) }}">
->>>>>>> benjamin
                                     @error('nombre')
                                     <span class="error text-danger">* {{ $message }}</span>
                                     @enderror
@@ -44,17 +33,10 @@
                                     <select class="form-control" name="categoria_id" id="categoria_id">
                                         <option value="">Seleccionar</option>
                                         @foreach ($categorias as $categoria)
-<<<<<<< HEAD
-                                            <option value="{{ $categoria['id'] }}"
-                                                @if ($categoria['id'] == old('categoria_id', $producto['categoria_id']))
-                                                selected
-                                                @endif
-                                            >
-=======
                                         <option value="{{ $categoria['id'] }}"
                                         @if ($categoria['id'] == old('categoria_id',
                                         $producto['categoria_id'])) selected @endif>
->>>>>>> benjamin
+
                                             {{ $categoria['nombre'] }}
                                         </option>
                                         @endforeach
@@ -102,7 +84,7 @@
                                         @if ($proveedor['id'] == old('proveedor_id', $producto['proveedor_id'])) selected @endif>
                                             {{ $proveedor['nombre'] }}
                                         </option>
->>>>>>> benjamin
+
                                         @endforeach
                                     </select>
                                     @error('proveedor_id')
@@ -119,13 +101,10 @@
                                             <span class="input-group-text">Bs</span>
                                         </div>
                                         <input type="number" class="form-control" placeholder="150,5"
-<<<<<<< HEAD
-                                            aria-label="Amount (to the nearest dollar)" name="precio_venta" id="precio_venta"
-                                            value="{{ old('precio_venta', $producto['precio_venta']) }}">
-=======
+
                                             aria-label="Amount (to the nearest dollar)" name="precio_venta"
                                             id="precio_venta" value="{{ old('precio_venta', $producto['precio_venta']) }}">
->>>>>>> benjamin
+
                                     </div>
                                     @error('precio_venta')
                                     <span class="error text-danger">* {{ $message }}</span>
@@ -143,13 +122,10 @@
                                             <span class="input-group-text">Bs</span>
                                         </div>
                                         <input type="number" class="form-control" placeholder="150,5"
-<<<<<<< HEAD
-                                            aria-label="Amount (to the nearest dollar)" name="precio_compra" id="precio_compra"
-                                            value="{{ old('precio_compra', $producto['precio_compra']) }}">
-=======
+
                                             aria-label="Amount (to the nearest dollar)" name="precio_compra"
                                             id="precio_compra" value="{{ old('precio_compra', $producto['precio_compra']) }}">
->>>>>>> benjamin
+
                                     </div>
                                     @error('precio_compra')
                                     <span class="error text-danger">* {{ $message }}</span>
@@ -162,12 +138,10 @@
                                     <label>Subir imagen</label>
                                     <div class="input-group">
                                         <div class="custom-file">
-<<<<<<< HEAD
-                                            <input type="file" class="custom-file-input" name="picture" id="imagen" lang="es" >
-=======
+
                                             <input type="file" class="custom-file-input" name="imagen" id="imagen"
                                                 lang="es">
->>>>>>> benjamin
+
                                             <label class="custom-file-label" for="imagen">Seleccionar imagen</label>
                                         </div>
                                     </div>
@@ -196,11 +170,7 @@
                                 Cancelar
                             </a>
                             <button class="btn btn-primary waves-effect waves-light" type="submit">
-<<<<<<< HEAD
-                                Guardar
-=======
                                 Actualizar
->>>>>>> benjamin
                             </button>
                         </div>
                     </form>
